9e10171d39e7cebc5053847c4755fe03e04fd6ab
Mann-Chandarana <mannchandarana@gmail.com>
2023-03-10 21:34:14 +0530

    Commiting new changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -14,9 +14,9 @@ export const Gallery = () => {
                     <div className="col-lg-12 d-flex justify-content-center">
                         <ul id="portfolio-flters">
                             <li data-filter="*" className="filter-active">All</li>
-                            <li data-filter=".filter-app">App</li>
-                            <li data-filter=".filter-card">Card</li>
-                            <li data-filter=".filter-web">Web</li>
+                            <li data-filter=".filter-app">Group</li>
+                            <li data-filter=".filter-card">contentest</li>
+                            <li data-filter=".filter-web">Seminar</li>
                         </ul>
                     </div>
                 </div>
@@ -26,84 +26,37 @@ export const Gallery = () => {
                     <div className="col-lg-4 col-md-6 portfolio-item filter-app">
                         <div className="portfolio-wrap">
                             <img style={{ height: "400px" }} src="assets/images/codepie_photos/1.jpg" className="img-fluid" alt="" />
-                            <div className="portfolio-info">
-                                <h4>App 1</h4>
-                                <p>App</p>
-                                <div className="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 1"><i className="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                </div>
-                            </div>
                         </div>
                     </div>
 
                     <div style={{ marginBottom: "-2rem" }} className="col-lg-4 col-md-6 portfolio-item filter-web">
                         <div className="portfolio-wrap">
                             <img style={{ height: "300px" }} src="assets/images/codepie_photos/2.jpg" className="img-fluid" alt="" />
-                            <div className="portfolio-info">
-                                <h4>Web 3</h4>
-                                <p>Web</p>
-                                <div className="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Web 3"><i className="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                </div>
-                            </div>
                         </div>
                     </div>
 
                     <div className="col-lg-4 col-md-6 portfolio-item filter-app">
                         <div className="portfolio-wrap">
                             <img style={{ height: "400px" }} src="assets/images/codepie_photos/3.jpeg" className="img-fluid" alt="" />
-                            <div className="portfolio-info">
-                                <h4>App 2</h4>
-                                <p>App</p>
-                                <div className="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 2"><i className="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
 
                     <div className="col-lg-4 col-md-6 portfolio-item filter-card">
-                        <div className="portfolio-wrap">
+                        <div style={{marginTop:"80px"}} className="portfolio-wrap">
                             <img src="assets/images/codepie_photos/4.jpg" className="img-fluid" alt="" />
-                            <div className="portfolio-info">
-                                <h4>Card 2</h4>
-                                <p>Card</p>
-                                <div className="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Card 2"><i className="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                </div>
-                            </div>
                         </div>
                     </div>
 
                     <div style={{ position: "relative", top: "-100px" }} className="col-lg-4 col-md-6 portfolio-item filter-app">
                         <div className="portfolio-wrap">
                             <img style={{ height: "330px" }} src="assets/images/codepie_photos/team.jpg" className="img-fluid" alt="" />
-                            <div className="portfolio-info">
-                                <h4>App 3</h4>
-                                <p>App</p>
-                                <div className="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 3"><i className="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                </div>
-                            </div>
                         </div>
                     </div>
 
                     <div className="col-lg-4 col-md-6 portfolio-item filter-web">
                         <div className="portfolio-wrap">
                             <img src="assets/images/codepie_photos/5.jpg" className="img-fluid" alt="" />
-                            <div className="portfolio-info">
-                                <h4>Web 2</h4>
-                                <p>Web</p>
-                                <div className="portfolio-links">
-                                    <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Web 2"><i className="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
