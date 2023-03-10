a85a049f27da6dda2afe67c1fa499597ce09b72a
Mann-Chandarana <mannchandarana@gmail.com>
2023-03-10 07:35:52 +0530

    Commiting new changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import { Register } from './components/Register';
 import { Footer } from './components/Footer';
@@ -31,6 +31,7 @@ function App() {
       this.classList.toggle('bi-list')
       this.classList.toggle('bi-x')
     })
+
     return () => {
       window.removeEventListener('scroll', handler);
     }
