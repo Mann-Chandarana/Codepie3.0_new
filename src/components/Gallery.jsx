b5ce60a21d143ab5973b8e3f1021cc57a8fe6fa7
BlazerHeat <vghoghari82@gmail.com>
2023-03-12 10:16:26 +0530

    Update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 
 export const Gallery = () => {
-    const handleclick = (name) => {
-        const value = document.getElementsByClassName(name);
-        console.log(value);
-    };
-
     return (
         <section id="Gallery" className="portfolio">
             <div className="container">
@@ -21,17 +16,10 @@ export const Gallery = () => {
                 <div className="row">
                     <div className="col-lg-12 d-flex justify-content-center">
                         <ul id="portfolio-flters">
-                            <li data-filter="*" className="">
+                            <li data-filter="*" className="filter-active">
                                 All
                             </li>
-                            <li
-                                onClick={() => {
-                                    handleclick('filter-app');
-                                }}
-                                data-filter=".filter-app"
-                            >
-                                Group
-                            </li>
+                            <li data-filter=".filter-app">Group</li>
                             <li data-filter=".filter-card">contentest</li>
                             <li data-filter=".filter-web">Seminar</li>
                         </ul>
@@ -43,7 +31,7 @@ export const Gallery = () => {
                         <div className="portfolio-wrap">
                             <img
                                 style={{ height: '400px' }}
-                                src="assets/images/codepie_photos/1.jpg"
+                                src="assets/images/codepie_photos/1.webp"
                                 className="img-fluid"
                                 alt=""
                             />
@@ -54,7 +42,7 @@ export const Gallery = () => {
                         <div className="portfolio-wrap">
                             <img
                                 style={{ height: '300px' }}
-                                src="assets/images/codepie_photos/2.jpg"
+                                src="assets/images/codepie_photos/2.webp"
                                 className="img-fluid"
                                 alt=""
                             />
@@ -65,7 +53,7 @@ export const Gallery = () => {
                         <div className="portfolio-wrap">
                             <img
                                 style={{ height: '400px' }}
-                                src="assets/images/codepie_photos/3.jpeg"
+                                src="assets/images/codepie_photos/3.webp"
                                 className="img-fluid"
                                 alt=""
                             />
@@ -74,7 +62,7 @@ export const Gallery = () => {
 
                     <div className="col-lg-4 col-md-6 portfolio-item filter-card">
                         <div style={{ marginTop: '80px' }} className="portfolio-wrap">
-                            <img src="assets/images/codepie_photos/4.jpg" className="img-fluid" alt="" />
+                            <img src="assets/images/codepie_photos/4.webp" className="img-fluid" alt="" />
                         </div>
                     </div>
 
@@ -85,7 +73,7 @@ export const Gallery = () => {
                         <div className="portfolio-wrap">
                             <img
                                 style={{ height: '330px' }}
-                                src="assets/images/codepie_photos/team.jpg"
+                                src="assets/images/codepie_photos/team.webp"
                                 className="img-fluid"
                                 alt=""
                             />
@@ -94,7 +82,7 @@ export const Gallery = () => {
 
                     <div className="col-lg-4 col-md-6 portfolio-item filter-web">
                         <div className="portfolio-wrap">
-                            <img src="assets/images/codepie_photos/5.jpg" className="img-fluid" alt="" />
+                            <img src="assets/images/codepie_photos/5.webp" className="img-fluid" alt="" />
                         </div>
                     </div>
                 </div>
